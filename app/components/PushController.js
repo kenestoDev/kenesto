bfc8c54a0f070c562babed0afef716003e102b96
kenestoDev <kenestodev@KenestoDevs-Mac.local>
2017-03-09 17:07:26 +0200

    iOS push in progress and icons

--- app/components/PushController.js
+++ app/components/PushController.js
@@ -1,10 +1,10 @@
 import React, { Component } from "react";
 
-import {Platform} from "react-native"
+import { Platform } from "react-native"
 
-import FCM, {FCMEvent, RemoteNotificationResult, WillPresentNotificationResult, NotificationType} from "react-native-fcm";
+import FCM, { FCMEvent, RemoteNotificationResult, WillPresentNotificationResult, NotificationType } from "react-native-fcm";
 
-import firebaseClient from  "./FirebaseClient";
+import firebaseClient from "./FirebaseClient";
 
 export default class PushController extends Component {
   constructor(props) {
@@ -12,6 +12,8 @@ export default class PushController extends Component {
   }
 
   componentDidMount() {
+
+    //FCM.subscribeToTopic('/topics/scottkenestodemo.com')
     FCM.requestPermissions();
 
     FCM.getFCMToken().then(token => {
@@ -25,33 +27,33 @@ export default class PushController extends Component {
     });
 
     this.notificationListner = FCM.on(FCMEvent.Notification, notif => {
-  
-     console.log("Notification", notif);
-      if(notif.local_notification){
+
+      console.log("Notification", notif);
+      if (notif.local_notification) {
         return;
       }
 
-      if(notif.opened_from_tray){
+      if (notif.opened_from_tray) {
         return;
       }
 
-      if(Platform.OS ==='ios'){
-              //optional
-              //iOS requires developers to call completionHandler to end notification process. If you do not call it your background remote notifications could be throttled, to read more about it see the above documentation link.
-              //This library handles it for you automatically with default behavior (for remote notification, finish with NoData; for WillPresent, finish depend on "show_in_foreground"). However if you want to return different result, follow the following code to override
-              //notif._notificationType is available for iOS platfrom
-              switch(notif._notificationType){
-                case NotificationType.Remote:
-                  notif.finish(RemoteNotificationResult.NewData) //other types available: RemoteNotificationResult.NewData, RemoteNotificationResult.ResultFailed
-                  break;
-                case NotificationType.NotificationResponse:
-                  notif.finish();
-                  break;
-                case NotificationType.WillPresent:
-                  notif.finish(WillPresentNotificationResult.All) //other types available: WillPresentNotificationResult.None
-                  break;
-              }
-            }
+      if (Platform.OS === 'ios') {
+        //optional
+        //iOS requires developers to call completionHandler to end notification process. If you do not call it your background remote notifications could be throttled, to read more about it see the above documentation link.
+        //This library handles it for you automatically with default behavior (for remote notification, finish with NoData; for WillPresent, finish depend on "show_in_foreground"). However if you want to return different result, follow the following code to override
+        //notif._notificationType is available for iOS platfrom
+        switch (notif._notificationType) {
+          case NotificationType.Remote:
+            notif.finish(RemoteNotificationResult.NewData) //other types available: RemoteNotificationResult.NewData, RemoteNotificationResult.ResultFailed
+            break;
+          case NotificationType.NotificationResponse:
+            notif.finish();
+            break;
+          case NotificationType.WillPresent:
+            notif.finish(WillPresentNotificationResult.All) //other types available: WillPresentNotificationResult.None
+            break;
+        }
+      }
       this.showLocalNotification(notif);
     });
 
@@ -62,11 +64,14 @@ export default class PushController extends Component {
   }
 
   showLocalNotification(notif) {
+    
+    alert(notif.data)
+  //  alert(notif.click_action)
     FCM.presentLocalNotification({
-      title: notif.title,
-      body: notif.body,
+      title: 'kuku', // notif.title,
+      body: 'zbabur', // notif.body,
       priority: "high",
-      click_action: notif.click_action,
+      click_action: "fcm.ACTION.HELLO", //notif.click_action,
       show_in_foreground: true,
       local: true
     });
@@ -75,6 +80,7 @@ export default class PushController extends Component {
   componentWillUnmount() {
     this.notificationListner.remove();
     this.refreshTokenListener.remove();
+    //FCM.unsubscribeToTopic('/topics/scottkenestodemo.com')
   }
 
 
